d7c088a7c375c4302fb564db46aaf7a0c2176c42
lizhibin88 <35754445@qq.com>
2019-11-01 19:38:05 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld1");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld2");
+		System.out.println("HelloWorld");
+	}
+}
